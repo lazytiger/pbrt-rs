a49b4108535f91b9f1fe391b2415930fa8de2204
hoping <baihaoping@gmail.com>
2021-03-23 15:40:54 +0800

    add lights

--- src/core/light.rs
+++ src/core/light.rs
@@ -2,12 +2,8 @@ use crate::core::geometry::Vector3f;
 use crate::core::interaction::SurfaceInteraction;
 use crate::core::spectrum::Spectrum;
 
-pub struct Light {}
+pub trait Light {}
 
-pub struct AreaLight {}
-
-impl AreaLight {
-    pub fn l(&self, si: &SurfaceInteraction, v: &Vector3f) -> Spectrum {
-        Spectrum {}
-    }
+pub trait AreaLight: Light {
+    fn l(&self, si: &SurfaceInteraction, v: &Vector3f) -> Spectrum;
 }
