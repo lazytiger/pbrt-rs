9cef90afa0740178f1ab96e7c34b110ffd0e44c5
hoping <baihaoping@gmail.com>
2021-04-09 11:48:31 +0800

    finish random

--- src/samplers/random.rs
+++ src/samplers/random.rs
@@ -1,33 +1,58 @@
 use crate::{
     core::{
-        geometry::Point2f,
+        geometry::{Point2f, Point2i},
         rng::RNG,
         sampler::{BaseSampler, Sampler},
     },
     impl_base_sampler,
 };
-use std::{sync::Arc};
+use std::sync::Arc;
 
+#[derive(Clone)]
 pub struct RandomSampler {
     base: BaseSampler,
     rng: RNG,
 }
 
-impl RandomSampler {}
+impl RandomSampler {
+    pub fn new(ns: usize, seed: usize) -> Self {
+        Self {
+            base: BaseSampler::new(ns as i64),
+            rng: RNG::new(seed),
+        }
+    }
+}
 
 impl Sampler for RandomSampler {
     impl_base_sampler!();
 
     fn get_1d(&mut self) -> f32 {
-        todo!()
+        self.rng.uniform_float()
     }
 
     fn get_2d(&mut self) -> Point2f {
-        todo!()
+        Point2f::new(self.rng.uniform_float(), self.rng.uniform_float())
     }
 
-    fn clone(&self, _seed: usize) -> Arc<Box<dyn Sampler>> {
-        todo!()
+    fn clone(&self, seed: usize) -> Arc<Box<dyn Sampler>> {
+        let mut rs = Clone::clone(self);
+        rs.rng.set_sequence(seed);
+        Arc::new(Box::new(rs))
+    }
+
+    fn start_pixel(&mut self, p: Point2i) {
+        for i in 0..self.sample_array_1d().len() {
+            for j in 0..self.sample_array_1d()[i].len() {
+                self.sample_array_1d_mut()[i][j] = self.rng.uniform_float();
+            }
+        }
+        for i in 0..self.sample_array_2d().len() {
+            for j in 0..self.sample_array_2d()[i].len() {
+                self.sample_array_2d_mut()[i][j] =
+                    Point2f::new(self.rng.uniform_float(), self.rng.uniform_float());
+            }
+        }
+        Sampler::start_pixel(self, p);
     }
 
     fn get_index_for_sample(&mut self, _sample_num: usize) -> i64 {
